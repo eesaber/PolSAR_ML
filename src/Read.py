a1b09d60d6a4802e58d1d2ebba7f68cf426a7338
EE527 <r05942013@ntu.edu.tw>
2017-10-20 21:26:04 +0800

    I/O function under construction

--- src/Read.py
+++ src/Read.py
@@ -1,18 +1,28 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import numpy as np
-import scipy.io as sio
-import matplotlib.pyplot as plt
 
-def Read():
-    path = '/media/akb/2026EF9426EF696C/raw_data/PiSAR2_07507_13170_009_131109_L090_CX_01_grd/'
-    #path = '../data/'
-    with open(path+'Covariance.mat', 'r') as file:
-        sig = sio.loadmat(file)
-        return sig
+import hdf5storage
+import numpy as np
+import matplotlib.pyplot as plt
+import os.path
+
+def Read(argv='single'):
+    opt = {'double':'Covariance_d.mat', 'single':'Covariance.mat', 'sparse':'Covariance_ds.mat'}
+    p = {'/media/akb/2026EF9426EF696C/raw_data/PiSAR2_07507_13170_009_131109_L090_CX_01_grd/', '../data/'}
+    for pat in p:
+        if os.path.isfile(pat+opt[argv]):
+            f = hdf5storage.loadmat(pat+opt[argv],'r')
+            #print(pat+opt[argv])
+            s = {}
+            for k,v in f.items():
+                print k
+                s[k] = np.array(v)
+            return s
+    print "Trainging data doesn't exist."
 
 if __name__=='__main__':
-    s = Read()
-    #sig = {'hh_hh': np.random.random((100,100))}
+    s = Read('single')
+    #s = {'hh_hh': np.random.random((100,100))}
     plt.figure(1)
     plt.imshow(10*np.log10(s['hh_hh']), cmap='jet', extent=[0,100,0,1], aspect='auto')
     plt.gca().invert_yaxis()
