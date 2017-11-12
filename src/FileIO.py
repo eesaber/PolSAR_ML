dc04d85aa56243978f150f96e258663650567242
EE527 <r05942013@ntu.edu.tw>
2017-11-12 16:40:55 +0800

    temp

--- src/FileIO.py
+++ src/FileIO.py
@@ -17,18 +17,21 @@ def read(argv='single'):
             for k,v in f.items():
                 s[k] = np.array(v)
             return s
-    print "Training data doesn't exist. The process is closed..."
+    print ("Training data doesn't exist. The process is closed...")
     sys.exit()
 
-if __name__=='__main__':
-    # DEBUG PART 
+def load_cut():
+    return {'train':(2400,2800,7200,8200), 'test':(1)}
 
+if __name__=='__main__':
+    # DEBUG 
     s = read('test')
     s['hh_hh'][s['hh_hh'] == 0] = 0.0000001
     plt.figure(1)
-    plt.imshow(10*np.log10(s['hh_hh'].T), cmap='jet', aspect='auto')
+    #plt.imshow(10*np.log10(s['hh_hh'][2800:3400, 8300:8855].T), cmap='jet', aspect='auto', clim = (-30,20))
+    plt.imshow(10*np.log10(s['hh_hh'].T[2400:2800, 7200:8200]), cmap='jet', aspect='auto', clim = (-30,20))
+ 
     plt.gca().invert_yaxis()
-    plt.clim((-30,20))
     plt.colorbar()
     plt.title('$S^2_{hh} (dB)$', fontsize=24)
     plt.show()
